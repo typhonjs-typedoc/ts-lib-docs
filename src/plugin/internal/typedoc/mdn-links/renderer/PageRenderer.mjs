b0bed9f4b2b750db817639caa606bf6b1e488a09
typhonrt <support@typhonjs.io>
2023-05-17 06:18:45 -0700

    Initial web component integration.

--- src/plugin/internal/typedoc/mdn-links/renderer/PageRenderer.mjs
+++ src/plugin/internal/typedoc/mdn-links/renderer/PageRenderer.mjs
@@ -1,9 +1,21 @@
-import { PageEvent } from 'typedoc';
+import fs                  from 'fs-extra';
+import path                from 'node:path';
+import { fileURLToPath }   from 'node:url';
 
-import { load }      from 'cheerio';
+import {
+   PageEvent,
+   RendererEvent }         from 'typedoc';
+
+import { load }            from 'cheerio';
+
+import { escapeAttr }      from '../utils.mjs';
 
 export class PageRenderer
 {
+   /** @type {import('typedoc').Application} */
+   #app;
+
+   /** @type {SymbolMaps} */
    #symbolMaps;
 
    /**
@@ -13,9 +25,11 @@ export class PageRenderer
     */
    constructor(app, symbolMaps)
    {
+      this.#app = app;
       this.#symbolMaps = symbolMaps;
 
-      app.renderer.on(PageEvent.END, this.#handlePageEnd, this);
+      this.#app.renderer.on(PageEvent.END, this.#handlePageEnd, this);
+      this.#app.renderer.once(RendererEvent.END, this.#handleRendererEnd, this);
    }
 
    /**
@@ -29,15 +43,39 @@ export class PageRenderer
    {
       // Get asset path to script by counting the number of `/` characters then building the relative path.
       const count = (page.url.match(/\//) ?? []).length;
-      const scriptPath = `${'../'.repeat(count)}assets/test.js`;
+      const scriptPath = `${'../'.repeat(count)}assets/mdn-web-components.js`;
 
-      // Create a new script tag.
-      const script = $('<script></script>');
-      script.attr('src', scriptPath);
-      script.attr('type', 'module');
+      // Append script to the head element.
+      $('head').append($(`<script src="${scriptPath}" type="module" />`));
+   }
 
-      // Append it to the head element.
-      $('head').append(script);
+   /**
+    * Modifications for every page.
+    *
+    * @param {import('cheerio').Cheerio}  $ -
+    */
+   #augmentGlobal($)
+   {
+      // Remove `Protected`, `Private`, and `External` from member visibility settings.
+      $('#tsd-filter-options li').each(function()
+      {
+         const el = $(this);
+         if (!el.find('input[name=inherited]').length) { el.remove(); }
+      });
+   }
+
+   /**
+    * Replaces the main header / title with a flex box element w/ mdn-links web component.
+    *
+    * @param {import('cheerio').Cheerio}  $ -
+    *
+    * @param {DataSymbolLinkInternal}     symbolDataInt -
+    */
+   #augmentTitle($, symbolDataInt)
+   {
+      $('.col-content').append($(`<wc-mdn-links data="${escapeAttr(symbolDataInt.mdnLinks)}" />`));
+
+      // $('.col-content').replaceWith($(`<wc-mdn-links data="${escapeAttr(symbolDataInt.mdnLinks)}" />`));
    }
 
    /**
@@ -47,20 +85,39 @@ export class PageRenderer
    {
       const symbolDataInt = this.#symbolMaps.internal.get(page.model);
 
-      if (symbolDataInt && page.contents)
-      {
-         // Load HTML content
-         const $ = load(page.contents);
+      const $ = load(page.contents);
 
+      this.#augmentGlobal($);
+
+      if (symbolDataInt)
+      {
          this.#addScript($, page);
 
-         console.log(`!! #handlePageEnd - A - has symbol data - name: ${symbolDataInt.name}; kind: ${page?.model?.kind}; url: ${page?.model?.url}`)
+         if (symbolDataInt.hasLinks) { this.#augmentTitle($, symbolDataInt); }
 
-         page.contents = $.html();
+         // console.log(`!! #handlePageEnd - A - has symbol data - name: ${symbolDataInt.name}; kind: ${page?.model?.kind}; url: ${page?.model?.url}`)
+
       }
       else
       {
-         console.log(`!! #handlePageEnd - B - NO symbol data - name: ${page?.model?.name}; kind: ${page?.model?.kind}; url: ${page?.model?.url}`)
+         // console.log(`!! #handlePageEnd - B - NO symbol data - name: ${page?.model?.name}; kind: ${page?.model?.kind}; url: ${page?.model?.url}`)
       }
+
+      page.contents = $.html();
+   }
+
+   /**
+    * Copy web components bundle to docs output assets directory.
+    */
+   #handleRendererEnd()
+   {
+      const outDir = `${this.#app.options.getValue('out')}${path.sep}assets`;
+      const localDir = path.dirname(fileURLToPath(import.meta.url));
+
+      this.#app.logger.verbose(`[mdn-links] Copying 'mdn-web-components.js' to output assets directory.`);
+
+      fs.copyFileSync(`${localDir}${path.sep}mdn-web-components.js`, `${outDir}${path.sep}mdn-web-components.js`);
+      fs.copyFileSync(`${localDir}${path.sep}mdn-web-components.js.map`,
+       `${outDir}${path.sep}mdn-web-components.js.map`);
    }
 }
